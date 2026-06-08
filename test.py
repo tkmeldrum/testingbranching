daa047c3fedbf6f2b1e717fa520e70915adfff64
acastillo73 <acastillo01@wm.edu>
2026-06-08 12:07:50 -0400

    add

---
+++ test.py
@@ -0,0 +1,12 @@
+def function(x,y):
+    z = x * y
+    return z
+def alexandra(y):
+    w = y - 7
+    return w
+def __main__():
+    x = 1
+    y = 2
+    z = function(x,y)
+    end = alexandra(y)
+    print(z - end)

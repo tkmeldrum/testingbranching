298ab3f9005ba7dad998ba4d24c65e3107e3dab9
tdfelton <feltontd5@gmail.com>
2026-06-08 12:07:43 -0400

    test py

---
+++ test.py
@@ -0,0 +1,11 @@
+def function(x,y):
+    x + y = z
+    return z
+def thomas(z,x):
+    return z*x
+def __main__():
+    x = 1
+    y = 2
+    z = function(x,y)
+    end = thomas(z,x)
+    print(end)
